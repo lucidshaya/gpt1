3cfa443fe698fad2429d8c9b163da56e10e70dda
Ohine.dev <126261826+lucidshaya@users.noreply.github.com>
2025-09-18 15:27:27 +0100

    1

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 // routes/userRoutes.js
 import express from 'express';
-import { registerUser, loginUser, getUser } from '../controllers/userController.js';
+import { registerUser, loginUser, getUser, getPublishedImages } from '../controllers/userController.js';
 import { protect } from '../middlewares/auth.js';
 
 const userRouter = express.Router();
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
-userRouter.get('/published-images', protect, getUser);
+userRouter.get('/published-images', getPublishedImages);
 
 export default userRouter;
